96b1ef764a7c7df2e046ca08a634c186808bcfde
Niall <niall.redmond@hotmail.com>
2016-11-03 17:37:32 +0000

    Initial Commit

---
+++ db/migrate/20161009133506_create_matches.rb
@@ -0,0 +1,12 @@
+class CreateMatches < ActiveRecord::Migration
+  def change
+    create_table :matches do |t|
+      t.string :match_name
+      t.boolean :result
+      t.integer :match_points
+      t.integer :student_id
+
+      t.timestamps
+    end
+  end
+end
